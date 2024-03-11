0898b5f9e9099ab5461a39a0e418b7514a752cea
sama <sama.hussien77@gmail.com>
2024-03-11 16:15:38 +0200

    update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
 
 #include <stdlib.h>
 
@@ -9,7 +7,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
@@ -22,4 +20,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif /* LISTS_H
+
