02ce3655d89b48fd405a8852340ba1cb1eaca4cd
xsky-hx <sama.hussien77@gmail.com>
2024-04-03 16:21:38 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    count = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
-            num += 1
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
         except IndexError:
-            break
+            continue
     print("")
-    return
+    return count
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
+            count = count + 1
+        except (TypeError, ValueError):
             pass
-    print()
-    Return
+    print("")
+    return count
